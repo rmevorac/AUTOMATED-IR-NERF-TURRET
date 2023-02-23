366e6f8498236c9d16edee5794faadf525501d69
LijYem <ermiasyemane91@gmail.com>
2023-02-22 17:32:42 -0800

    code from previous labs

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import pyb
+import utime as time
+from machine import Pin, I2C
+from mlx90640 import MLX90640
+from mlx90640.calibration import NUM_ROWS, NUM_COLS, IMAGE_SIZE, TEMP_K
+from mlx90640.image import ChessPattern, InterleavedPattern
+from mma8451.mma845x import *
+
+
+if __name__ == "__main__":
+    
+    ## The creation of the motor 1 object
+    motor1 = MotorDriver(Pin.board.PC1, Pin.board.PA0, Pin.board.PA1, 5)
+
+    ## The creation of the motor 2 object
+    motor2 = MotorDriver(Pin.board.PA10, Pin.board.PB4, Pin.board.PB5, 3)
+
+    ## The creation of the encoder 1 object
+    encoder1 = Encoder(Pin.board.PB6, Pin.board.PB7, 4)
+
+    ## The creation of the encoder 2 object
+    encoder2 = Encoder(Pin.board.PC6, Pin.board.PC7, 8)
+    # The following import is only used to check if we have an STM32 board such
+    # as a Pyboard or Nucleo; if not, use a different library
+    try:
+        from pyb import info
+
+    # Oops, it's not an STM32; assume generic machine.I2C for ESP32 and others
+    except ImportError:
+        # For ESP32 38-pin cheapo board from NodeMCU, KeeYees, etc.
+        i2c_bus = I2C(1, scl=Pin(22), sda=Pin(21))
+
+    # OK, we do have an STM32, so just use the default pin assignments for I2C1
+    else:
+        i2c_bus = I2C(1)
+
+    print("MXL90640 Easy(ish) Driver Test")
+
+    # Select MLX90640 camera I2C address, normally 0x33, and check the bus
+    i2c_address = 0x33
+    scanhex = [f"0x{addr:X}" for addr in i2c_bus.scan()]
+    print(f"I2C Scan: {scanhex}")
+
+    # Create the camera object and set it up in default mode
+    camera = MLX_Cam(i2c_bus)
+
