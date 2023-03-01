44fbc1d9d9407218cf03086a92975779be681013
rmevorac <rmevorac@calpoly.edu>
2023-02-28 23:11:05 -0800

    Task Diagram V1

--- src/test_main.py
+++ src/test_main.py
@@ -1,14 +1,49 @@
 import pyb
+import gc
 import utime as time
-from machine import Pin, I2C
+from pyb import Pin as Pin
+#from machine import Pin, I2C
+
 from mlx90640 import MLX90640
 from mlx90640.calibration import NUM_ROWS, NUM_COLS, IMAGE_SIZE, TEMP_K
 from mlx90640.image import ChessPattern, InterleavedPattern
 from mma8451.mma845x import *
 
+from task.cotask import cotask
+from task.task_share import task_share
+from motor_driver.encoder_reader import Encoder
+from motor_driver.motor_driver import MotorDriver
+from motor_driver.controller import Controller
 
-#if __name__ == "__main__":
-    
+def get_accel(shares):
+    """!
+    Task that gets x, y, and z acceleration from the accelerometer
+    @param shares A list holding the share and queue used by this task
+    """
+    # Get references to the share and queue which have been passed to this task
+    my_share, my_queue = shares
+
+    mma.active()
+    while True:
+        x,y,z= mma.get_accels()
+        my_queue.put(x)
+        my_queue.put(y)
+        my_queue.put(z)
+
+        yield 0
+
+if __name__ == "__main__":
+    ## Set kp and setpoints for controller
+    kp1, kp2, sp1, sp2 = .01, .01, 1000, 1000
+
+    ## Create a share and a queue to test function and diagnostic printouts
+    share0 = task_share.Share('h', thread_protect=False, name="Share 0")
+    q0 = task_share.Queue('h', 16, thread_protect=False, overwrite=False,
+                          name="Queue 0")
+
+    ## Create accelerometer object with 
+    mma = MMA845x(pyb.I2C(1, pyb.I2C.MASTER, baudrate = 100000), 29)
+
     ## The creation of the motor 1 object
     motor1 = MotorDriver(Pin.board.PC1, Pin.board.PA0, Pin.board.PA1, 5)
 
@@ -20,6 +55,45 @@ from mma8451.mma845x import *
 
     ## The creation of the encoder 2 object
     encoder2 = Encoder(Pin.board.PC6, Pin.board.PC7, 8)
+
+    ## Once motor, encoder and params are collected they are used to create this controller 1 object
+    controller1 = Controller(kp1, sp1, motor1, encoder1)
+
+    ## Once motor, encoder and params are collected they are used to create this controller 2 object
+    controller2 = Controller(kp2, sp2, motor2, encoder2)
+
+    # Create the tasks. If trace is enabled for any task, memory will be
+    # allocated for state transition tracing, and the application will run out
+    # of memory after a while and quit. Therefore, use tracing only for 
+    # debugging and set trace to False when it's not needed
+    task1 = cotask.Task(get_accel, name="Task_1", priority=2, period=500,
+                        profile=True, trace=False, shares=(share0, q0))
+    task2 = cotask.Task(task2_fun, name="Task_2", priority=1, period=500,
+                        profile=True, trace=False, shares=(share0, q0))
+    cotask.task_list.append(task1)
+    cotask.task_list.append(task2)
+
+    # Run the memory garbage collector to ensure memory is as defragmented as
+    # possible before the real-time scheduler is started
+    gc.collect()
+
+    # Run the scheduler with the chosen scheduling algorithm. Quit if ^C pressed
+    while True:
+        try:
+            cotask.task_list.pri_sched()
+        except KeyboardInterrupt:
+            break
+
+    # Print a table of task data and a table of shared information data
+    print('\n' + str (cotask.task_list))
+    print(task_share.show_all())
+    print(task1.get_trace())
+    print('')
+
+
+
+
+
     # The following import is only used to check if we have an STM32 board such
     # as a Pyboard or Nucleo; if not, use a different library
     try:
@@ -34,13 +108,11 @@ from mma8451.mma845x import *
     else:
         i2c_bus = I2C(1)
 
-    print("MXL90640 Easy(ish) Driver Test")
-
     # Select MLX90640 camera I2C address, normally 0x33, and check the bus
     i2c_address = 0x33
     scanhex = [f"0x{addr:X}" for addr in i2c_bus.scan()]
     print(f"I2C Scan: {scanhex}")
 
-    # Create the camera object and set it up in default mode
+    ## Create the camera object and set it up in default mode
     camera = MLX_Cam(i2c_bus)
 
