73594ffe03d0d0512fa7331e064c8a879216a030
rmevorac <rmevorac@calpoly.edu>
2023-03-20 22:15:28 -0700

    doxygen files

---
+++ docs/main_8py.js
@@ -0,0 +1,23 @@
+var main_8py =
+[
+    [ "button_press", "main_8py.html#a03926457883535409d0c041d2d5067f8", null ],
+    [ "fire_round", "main_8py.html#a95bb9805fca5742f998995806406f98b", null ],
+    [ "get_coordinates", "main_8py.html#a6546cd348d8aee34fc5e65078e754b01", null ],
+    [ "move_pitch_motor", "main_8py.html#a978b7826d36094faacc9d762fa90c5ff", null ],
+    [ "move_yaw_motor", "main_8py.html#aa4f02459c0fe399b8ea717306ac7212f", null ],
+    [ "button_count", "main_8py.html#ac674b539cab0492047a2d86fd4295030", null ],
+    [ "button_pin", "main_8py.html#a1fad54c990700569eeebb03616fdc978", null ],
+    [ "camera", "main_8py.html#a4d70559af0a6ba9fcc2b474c0c18d357", null ],
+    [ "controller1", "main_8py.html#a46a5d5062114b708b96b0850aca34b19", null ],
+    [ "controller2", "main_8py.html#a34faa3a3f3492c9c24db3aa7028e2057", null ],
+    [ "encoder1", "main_8py.html#a87fdac18f7a1444924085943a8234e19", null ],
+    [ "encoder2", "main_8py.html#a49453f9104c958c58a1bd110314ce2c8", null ],
+    [ "i2c_address", "main_8py.html#afab24b775ca486fc25931509f42ccce1", null ],
+    [ "kp1", "main_8py.html#a6ea1a0cb7c3921cec09035e3c435f11c", null ],
+    [ "kp2", "main_8py.html#abb30fbd26f26969e10ab3652538118b2", null ],
+    [ "motor1", "main_8py.html#a199100edd0090728cb79f1505b6a2743", null ],
+    [ "motor2", "main_8py.html#aa6bc7132ff8b65d56111c82d2976ff8c", null ],
+    [ "q0", "main_8py.html#ae956baf22be01b82f8f5cc7ffc167d69", null ],
+    [ "share0", "main_8py.html#a3ebd9bb4d62bde9a647a803917e0e434", null ],
+    [ "trig_pin", "main_8py.html#a295f9ce3fdea8fb64223ed7dfc2d67b7", null ]
+];
