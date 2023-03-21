883f4fc362de617e446c2941000c3b53108843ca
rmevorac <rmevorac@calpoly.edu>
2023-03-20 22:43:14 -0700

    updated doxygen files

---
+++ docs/index.js
@@ -0,0 +1,33 @@
+var index =
+[
+    [ "AUTOMATED IR NERF TURRET MECHA 20", "index.html#autotoc_md1", [
+      [ "Introduction", "index.html#autotoc_md3", null ]
+    ] ],
+    [ "Turret Design", "index.html#autotoc_md5", [
+      [ "Hardware Design", "index.html#autotoc_md6", [
+        [ "General View:", "index.html#autotoc_md7", null ],
+        [ "Yaw Axis", "index.html#autotoc_md8", null ],
+        [ "Pitch Axis", "index.html#autotoc_md9", null ],
+        [ "Gun Pivot", "index.html#autotoc_md10", null ],
+        [ "Firing Access Modification", "index.html#autotoc_md11", null ],
+        [ "Electronic Design", "index.html#autotoc_md12", null ]
+      ] ],
+      [ "Software Design", "index.html#autotoc_md13", [
+        [ "Task Diagram", "index.html#autotoc_md14", null ],
+        [ "Finite State Diagrams", "index.html#autotoc_md15", null ]
+      ] ]
+    ] ],
+    [ "Project Outcome", "index.html#autotoc_md17", [
+      [ "Duel Day Performance", "index.html#autotoc_md18", [
+        [ "Testing Results", "index.html#autotoc_md19", null ]
+      ] ],
+      [ "What We Learned", "index.html#autotoc_md20", [
+        [ "Dos:", "index.html#autotoc_md21", null ],
+        [ "Donts:", "index.html#autotoc_md22", null ],
+        [ "Mechanical Lessons", "index.html#autotoc_md23", null ],
+        [ "Eletrical Lessons", "index.html#autotoc_md24", null ],
+        [ "Software Lessons", "index.html#autotoc_md25", null ]
+      ] ]
+    ] ],
+    [ "Original Description and additional files", "index.html#autotoc_md27", null ]
+];
